5fa7b0143955a78bbbe38424fde0e9ba6c4d9516
Arjun8991 <51797405+Arjun8991@users.noreply.github.com>
2019-10-08 18:50:31 +0530

    Update elab_print_os_3rdyear_ktr.py

--- elab_print_os_3rdyear_ktr.py
+++ elab_print_os_3rdyear_ktr.py
@@ -5,16 +5,16 @@ import argparse
 
 def login(sess,user,pas):
     creds='userid=%s&password=%s' %(user,pas)
-    url="http://care.srmuniv.ac.in/ktrstudentskill/index.helper.php"
+    url="http://care.srmist.edu.in/rmpds/student/questions"
     header={
-        'Host': 'care.srmuniv.ac.in',
+        'Host': 'care.srmist.edu.in',
         'Content-Length': '42',
         'Accept': '*/*',
-        'Origin': 'http://care.srmuniv.ac.in',
+        'Origin': 'http://care.srmist.edu.in',
         'X-Requested-With': 'XMLHttpRequest',
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Referer': 'http://care.srmuniv.ac.in/ktrstudentskill/index.php',
+        'Referer': 'http://care.srmist.edu.in/rmpds/student/questions',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
         'Cookie': '%s' %(sess[0]),
@@ -35,9 +35,9 @@ if __name__ == "__main__":
     login(sess=sess,user=args.username,pas=args.password)
 
     for i in range (0,100):
-        ref='http://care.srmuniv.ac.in/ktrstudentskill/login/studentnew/code/operating-systems/operating-systems.code.php?id=1&value=9'
+        ref='http://care.srmist.edu.in/rmpds/student/questions'
         headers={
-        'Host': 'care.srmuniv.ac.in',
+        'Host': 'care.srmist.edu.in',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
@@ -59,7 +59,7 @@ if __name__ == "__main__":
             'Connection': 'close'
         }
 
-        url1='http://care.srmuniv.ac.in/ktrstudentskill/login/studentnew/code/operating-systems/operating-systems.code.php?id=1&value=%d' %i
+        url1='http://care.srmist.edu.in/rmpds/student/questions.php?id=1&value=%d' %i
         url='http://care.srmuniv.ac.in/ktrstudentskill/login/studentnew/code/getReport.php'
         print("Dowloading Report:%d" %i )
         r=requests.request("GET",url1,headers=header2)
